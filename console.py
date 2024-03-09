d0b649e3db44adb2c2901e9ac4d02abef5497f47
ochuks1 <cochuks@gmail.com>
2024-03-09 04:00:38 -0800

    Abegg now

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ Contains the entry point of the command interpreter.
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
